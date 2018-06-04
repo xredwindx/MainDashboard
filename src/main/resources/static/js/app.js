4c2745cefb517528026ae851ecac711f42a72605
sichoon.kwon <sichoon.kwon@solbox.com>
2018-06-04 16:53:53 +0900

    responsive table dev

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,4 +2,4 @@
  * Created by ??? on 2017-12-29.
  */
 'use strict'
-var app = angular.module("myApp", ["ui.router", "ui.bootstrap"]);
+var app = angular.module("myApp", ["ui.router", "ui.bootstrap", "wt.responsive"]);
